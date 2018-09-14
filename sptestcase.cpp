e0c32a92f66816a1508abc8ce0476a65a214c2ef
Bruce <BruceLuu007@gmail.com>
2018-09-14 14:45:44 +0800

    testcase2()

--- sptestcase.cpp
+++ sptestcase.cpp
@@ -18,8 +18,27 @@ void testcase1(){
 	SmartPointer<const char> spstr = "Hello world!";
 }
 
+
+void testcase2(){
+	SmartPointer<SomeClass> spclass1 = new SomeClass;
+	std::cout << std::endl;
+
+	spclass1 = spclass1;
+	std::cout << std::endl;
+
+	// 创建另一个智能指针，与先前的指针指向同一块内存资源
+	SmartPointer<SomeClass> spclassother = spclass1;
+	std::cout << std::endl;
+
+	// 先创建智能指针,再用另一个智能指针给已创建好的智能指针赋值
+	SmartPointer<SomeClass> spclass2 = new SomeClass;
+	std::cout << std::endl;
+	spclass2 = spclass1;
+	std::cout << std::endl;
+}
+
 int main(void){
-	testcase1();
+	testcase2();
 	while (1);
 	return 0;
 }
