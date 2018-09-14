50ed9de51690e657dd864b7267f5c8e1cae57c85
Bruce <BruceLuu007@gmail.com>
2018-09-14 15:04:32 +0800

    断点调试 加入注释

--- sptestcase.cpp
+++ sptestcase.cpp
@@ -20,20 +20,20 @@ void testcase1(){
 
 
 void testcase2(){
-	SmartPointer<SomeClass> spclass1 = new SomeClass;
+	SmartPointer<SomeClass> spclass1 = new SomeClass;//調用SmartPointer(T *p) :mPointer(p)
 	std::cout << std::endl;
 
 	spclass1 = spclass1;
 	std::cout << std::endl;
 
 	// 创建另一个智能指针，与先前的指针指向同一块内存资源
-	SmartPointer<SomeClass> spclassother = spclass1;
+	SmartPointer<SomeClass> spclassother = spclass1;//调用SmartPointer(const SmartPointer &other) :mPointer(other.mPointer)
 	std::cout << std::endl;
 
 	// 先创建智能指针,再用另一个智能指针给已创建好的智能指针赋值
 	SmartPointer<SomeClass> spclass2 = new SomeClass;
 	std::cout << std::endl;
-	spclass2 = spclass1;
+	spclass2 = spclass1;//调用SmartPointer &operator = 
 	std::cout << std::endl;
 }
 
